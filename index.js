19e1cc6f5cebde755da43be3955129dc427d39f0
Maria Jenkins <macjenkin@gmail.com>
2023-03-08 17:14:05 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    const cards = [];
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+        debugger;
+    }
+    
+    return cards;
+}
+
+function countDown(positiveInteger) {
+    while (positiveInteger >= 0) {
+        console.log(positiveInteger--);
+    }
+}
