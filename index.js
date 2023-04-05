ea24197be495c3e5e80f58673ed2bd267695f447
Maria Jenkins <macjenkin@gmail.com>
2023-04-05 07:51:34 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function writeCards(names, eventName) {
-    const cards = [];
+    let cards = [];
     for (let i = 0; i < names.length; i++) {
-        cards.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
-        debugger;
-    }
-    
-    return cards;
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+    };
+    return cards
 }
 
-function countDown(positiveInteger) {
-    while (positiveInteger >= 0) {
-        console.log(positiveInteger--);
+function countDown(num) {
+    let i = num;
+    while (i >= 0) {
+        console.log(i);
+        i--;
     }
 }
